f50df97eb83c6cbae91bab12e0e68819178dc652
YI DING <yid3@student.unimelb.edu.au>
2018-05-16 18:21:31 +1000

    Distributed Function and create object from animation profile

--- pddl.py
+++ pddl.py
@@ -7,6 +7,6 @@ req = urllib2.Request('http://solver.planning.domains/solve')
 req.add_header('Content-Type', 'application/json')
 response=urllib2.urlopen(req, json.dumps(data)).read()
 resp = json.loads(response)
-print(resp)
+print(response)
 with open('result.json', 'w') as f:
     f.write(response)
